8788d9735e456ce241e8a9931af6cb0e265d27a3
sowmiya <sowmiyanallasivam3434@gmail.com>
2023-11-09 18:33:59 +0530

    Header and Sidebar changed

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Analysis from "../../assets/Analysis.png";
 import Watchlist from '../../assets/Watchlist.png';
+import Trending from '../../assets/Trending.jpg'
 
 function Sidebar() {
   return (
     <div className="w-64 bg-gray-800 text-white h-screen flex items-center justify-center">
       <ul>
-        <li className="hover:bg-gray-600 mb-5">Trending</li>
+        <img className='w-12 h-12 bg-blue-100 rounded-full p-2 mb-5 hover:bg-gray-600' src={Trending} />
         <img className='w-12 h-12 bg-blue-100 rounded-full p-2 mb-5 hover:bg-gray-600' src={Analysis} alt="Analysis Icon" />
         <img className='w-12 h-12 bg-blue-100 rounded-full p-2 hover:bg-gray-600' src={Watchlist} alt="Watchlist Icon" />
       </ul>
